e2e1911c0327bc05603b6f0f50d025b4c75b1333
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-27 06:15:02 +0300

    start array

---
+++ task/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите количество строк предполагаемого массива: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
+Console.Write("Введите количество символов строки массива, которое хотите вывести на экран: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int size = m;
+string[] array = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите строку №{0}:   ", i + 1);
+    array[i] = Console.ReadLine(); //Заполняем его элементами введенными с клавиатуры
+    
+}
+    Console.WriteLine();
+    Console.Write("Start array-> [");
+    Console.Write(String.Join(",", array));Console.Write("]"); 
