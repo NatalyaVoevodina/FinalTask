607e3c34d33dc4f085f6b833c8547b264fad2546
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-27 06:17:41 +0300

    check array

--- task/Program.cs
+++ task/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.Write("Введите количество строк предполагаемого массива: ");
+﻿int ArrayCheck(string[] array, int size)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= size) count++;
+    }
+    return count;
+
+
+Console.Write("Введите количество строк предполагаемого массива: ");
 int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число.
 Console.Write("Введите количество символов строки массива, которое хотите вывести на экран: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -13,3 +23,5 @@ for (int i = 0; i < array.Length; i++)
     Console.WriteLine();
     Console.Write("Start array-> [");
     Console.Write(String.Join(",", array));Console.Write("]"); 
+
+    int count = ArrayCheck(array, size);
